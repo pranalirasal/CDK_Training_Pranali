cf801327886c8f96b2c4e6ea652df0a468ba65be
pranalirasal <pranalirasal@gmail.com>
2017-07-25 14:30:10 +0530

    Process modified for enum year

--- day5/Process.java
+++ day5/Process.java
@@ -2,7 +2,7 @@ public class Process {
 	public static void main (String[] args) {
 		processEntry();
 	}
-	@TimeStamp (day = Day.MON, month = Month.JUL, year = Year.SEVENTEEN)
+	@TimeStamp (day = Day.MON, month = Month.JUL, year = Year._2017)
 	public static void processEntry () {
 		System.out.println("Annotation Demo at method and class level.");
 	}
